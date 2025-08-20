7da59c0c712a0c62a0493f00b08b4a978840994a
Jamínteles Desus Ribeiro Moura <devjaminteles@gmail.com>
2025-08-20 20:34:55 -0300

    A

---
+++ .2/Exercicios/Atv1.java
@@ -0,0 +1,11 @@
+public class Atv1 {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        System.out.println("Elementos no array:");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Posição " + i + ": " + numeros[i]);
+        }
+    }
+}
