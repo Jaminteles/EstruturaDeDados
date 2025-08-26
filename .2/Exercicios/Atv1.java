482b3f0038fcb010c74c6fecadb87842de3f1686
Jamínteles Desus Ribeiro Moura <devjaminteles@gmail.com>
2025-08-26 18:02:25 -0300

    .

--- .2/Exercicios/Atv1.java
+++ .2/Exercicios/Atv1.java
@@ -1,3 +1,5 @@
+// Q1)
+
 public class Q1 {
     public static void main(String[] args) {
         int[] numeros = {1, 2, 3, 4, 5};
@@ -9,7 +11,7 @@ public class Q1 {
     }
 }
 
-//Q2)
+// Q2)
 
 import java.util.Scanner;
 
@@ -18,16 +20,16 @@ public class Q2 {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-
         for (int i = 0; i < N; i++){
             quadrados[i] = (i + 1) * (i + 1);
         }
-
         System.out.println("Array de quadrados de 1 até " + N + ":");
         for int i = 0; i < quadrados.length; i++) {
-            
+            System.out.println("Posição " + i + " -> " + quadrados[i]);
         }
+        sc.close();
     }
 }
 
+// Q3)
 
