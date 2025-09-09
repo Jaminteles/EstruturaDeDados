448fda5f685f1d57e586133b41cccc8ecd033de4
Jamínteles Desus Ribeiro Moura <devjaminteles@gmail.com>
2025-09-09 19:46:56 -0300

    Q8

---
+++ .2/Exercicios/Q8.java
@@ -0,0 +1,44 @@
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] idade = new int[5];
+        double[] altura = new double[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Insira a idade: ");
+            idade[i] = sc.nextInt();
+            System.out.println("Insira a altura: ");
+            altura[i] = sc.nextDouble();
+        }
+        //idade
+        for (int i = 4; i >= 0; i--) {
+            if (i == 4) {
+                System.out.print("Idade: ");
+                System.out.print("[");
+            }
+            if (i != 0) {
+                System.out.print(idade[i] + ", ");
+            } else {
+                System.out.print(idade[i]);
+            }
+            if (i == 0) {
+                System.out.println("]");
+            }
+        }
+        //Altura
+        for (int i = 4; i >= 0; i--) {
+            if (i == 4) {
+                System.out.print("Altura: ");
+                System.out.print("[");
+            }
+            if (i != 0) {
+                System.out.print(altura[i] + ", ");
+            } else {
+                System.out.print(altura[i]);
+            }
+            if (i == 0) {
+                System.out.println("]");
+            }
+        }
+    }
+}
